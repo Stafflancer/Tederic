a2d390fa36a92a5b9ec900a722ef25f282962c7f
Sasha <sasha.chyrskyy@gmail.com>
2020-10-15 11:54:58 +0300

    gulp. added styles

---
+++ web/themes/custom/tederic/gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  return gulp.src('sass/main.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('css'))
+});
+
+// Added autoprefixer for css style
+gulp.task('autoprefix', () =>
+  gulp.src('css/main.css')
+    .pipe(autoprefixer({
+      browsers: ['last 99 versions'],
+      cascade: false
+  }))
+  .pipe(gulp.dest('css'))
+);
+
+gulp.task('watch', function() {
+  gulp.watch('sass/*.scss', gulp.series('sass'));
+});
