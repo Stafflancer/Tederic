4df05ac58e1b47b54873178376c070c880dfc75c
Sasha <sasha.chyrskyy@gmail.com>
2020-10-17 21:03:13 +0300

    added style (Home page)

---
+++ web/themes/custom/tederic/js/script.js
@@ -0,0 +1,44 @@
+(function($, Drupal) {
+  $(document).ready(function() {
+
+    // Main slider
+    var $status = $('.slick-pagination .numbers');
+    var $slickElement = $('.slider-section .field--name-field-sliders');
+
+    $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+        //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+        var i = (currentSlide ? currentSlide : 0) + 1;
+        $status.text(i + '/' + slick.slideCount);
+    });
+
+    $slickElement.not('.slick-initialized').slick({
+      arrows: true,
+      infinite: false,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      autoplay: true,
+      pauseOnHover: true,
+      autoplaySpeed: 5000,
+      appendArrows: $('.main-slick-arrows'),
+      prevArrow: '<div class="main-arrow-prev" aria-hidden="true"></div>',
+      nextArrow: '<div class="main-arrow-next" aria-hidden="true"></div>',
+    });
+
+    // Slider for icons (Home page)
+    $('.icons-section .field--name-field-icons-sliders').not('.slick-initialized').slick({
+      arrows: true,
+      infinite: false,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      adaptiveHeight: true,
+    });
+
+  });
+  Drupal.behaviors.script = {
+    attach: function (context, settings) {
+
+      
+
+    }
+  }
+})(jQuery, Drupal);
