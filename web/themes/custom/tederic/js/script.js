6218dbd7ebb7b73ced94f029fa85089e3558249b
Sasha <sasha.chyrskyy@gmail.com>
2020-10-23 11:53:41 +0300

    styles

--- web/themes/custom/tederic/js/script.js
+++ web/themes/custom/tederic/js/script.js
@@ -3,26 +3,51 @@
 
     // Main slider
     var $status = $('.slick-pagination .numbers');
-    var $slickElement = $('.slider-section .field-sliders');
 
-    $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+    function slickCount(slickElement) {
+      slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
         //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
         var i = (currentSlide ? currentSlide : 0) + 1;
         $status.text(i + '/' + slick.slideCount);
-    });
+      });
+    }
 
-    $slickElement.not('.slick-initialized').slick({
-      arrows: true,
-      infinite: false,
-      slidesToShow: 1,
-      adaptiveHeight: true,
-      autoplay: true,
-      pauseOnHover: true,
-      autoplaySpeed: 5000,
-      appendArrows: $('.main-slick-arrows'),
-      prevArrow: '<div class="main-arrow-prev" aria-hidden="true"></div>',
-      nextArrow: '<div class="main-arrow-next" aria-hidden="true"></div>',
-    });
+    if ($('.slider-section .field-sliders').length > 0) {
+      var $slickElement = $('.slider-section .field-sliders');
+      slickCount($slickElement);
+
+      $slickElement.not('.slick-initialized').slick({
+        arrows: true,
+        infinite: false,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        pauseOnHover: true,
+        autoplaySpeed: 5000,
+        appendArrows: $('.main-slick-arrows'),
+        prevArrow: '<div class="main-arrow-prev" aria-hidden="true"></div>',
+        nextArrow: '<div class="main-arrow-next" aria-hidden="true"></div>',
+      });
+    } else if ($('.solutions-part .field-images').length > 0) {
+      var $slickElement = $('.solutions-part .field-images');
+      slickCount($slickElement);
+
+      $slickElement.not('.slick-initialized').slick({
+        arrows: true,
+        infinite: true,
+        slidesToShow: 2,
+        adaptiveHeight: true,
+        autoplay: true,
+        pauseOnHover: true,
+        centerMode: true,
+        autoplaySpeed: 5000,
+        appendArrows: $('.slick-pagination'),
+        prevArrow: '<div class="main-arrow-prev" aria-hidden="true"></div>',
+        nextArrow: '<div class="main-arrow-next" aria-hidden="true"></div>',
+      });
+    }
+
+    console.log($('.solutions-part .field-images').length > 0);
 
     // Slider for icons (Home page)
     $('.icons-section .field-icons-sliders').not('.slick-initialized').slick({
