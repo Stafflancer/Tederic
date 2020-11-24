7631bf5a9947136839bf6e325f76548e90da7d80
andriy <andriy@stafflancer.com>
2020-11-24 17:44:35 +0200

    Map changes.

---
+++ web/themes/custom/tederic/js/map/load-map-scripts.js
@@ -0,0 +1,34 @@
+(function($, Drupal, drupalSettings) {
+  
+  var added = false;
+  $(document).scroll(function() {
+    if (!added && $(document).scrollTop()) {
+      
+      $.getScript(location.origin + '/themes/custom/tederic/js/leaflet/leaflet.js')
+      .done(function( script, textStatus ) {
+
+        $.getScript(location.origin + '/themes/custom/tederic/js/leaflet/leaflet.responsive.popup.js')
+          .done(function( script, textStatus ) {
+
+            $.getScript(location.origin + '/themes/custom/tederic/js/imgViewer2/dist/imgViewer2.min.js')
+
+              .done(function( script, textStatus ) {
+                $.getScript(location.origin + '/themes/custom/tederic/js/map/tederic-map.js');
+                $('head').append('<link rel="stylesheet" href="' + location.origin + '/themes/custom/tederic/js/imgViewer2/dist/imgViewer2.min.css" type="text/css" />');
+
+                $('head').append('<link rel="stylesheet" href="' + location.origin + '/themes/custom/tederic/js/leaflet/leaflet.css" type="text/css" />');
+
+                $('head').append('<link rel="stylesheet" href="' + location.origin + '/themes/custom/tederic/js/leaflet/leaflet.responsive.popup.css" type="text/css" />');
+
+                $('head').append('<link rel="stylesheet" href="' + location.origin + '/themes/custom/tederic/css/tederic-map.css" type="text/css" />');
+              });
+
+          });
+        
+      });
+
+      added = true;
+    }
+  });
+
+})(jQuery, Drupal, drupalSettings);
